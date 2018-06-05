697fce578392b8d0954b777d20a11f49e1007071
Serere Jegede <sererejegede@gmail.com>
2018-06-05 16:36:48 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-toolbar>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title>Project Management</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat>Users</v-btn>
+        <v-btn flat>Companies</v-btn>
+        <v-btn flat>Projects</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main>
+
+    </main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  name: 'App'
+}
+</script>
